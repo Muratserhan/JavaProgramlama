5fbbd193f369d64db106e97dc5d15720e8f0a663
Muratserhan <101399604+Muratserhan@users.noreply.github.com>
2022-04-11 13:21:47 +0300

    Delete rentACar directory

--- rentACar/core/crossCuttingConcers/exceptionHandling/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.etiya.rentACar.core.crossCuttingConcers.exceptionHandling;
-
-@SuppressWarnings("serial")
-public class BusinessException extends RuntimeException {
-	public BusinessException(String message) {
-		super(message);
-	}
-}
--- rentACar/dataAccess/abstracts/StateDao.java
+++
@@ -1,9 +0,0 @@
-package com.etiya.rentACar.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.etiya.rentACar.entities.State;
-
-public interface StateDao extends JpaRepository<State, Integer> {
-
-}
